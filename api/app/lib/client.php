a9748a328a4a69d5f885e5a0b1027d108c673406
jrubendiaz <jj.rubendiaz@gmail.com>
2018-01-18 16:21:24 +0000

    API REST updated
    delete method created + bug fixed

--- api/app/lib/client.php
+++ api/app/lib/client.php
@@ -3,95 +3,113 @@
 /*
 *   Get all clients in the system
 */
-function getAllClients() {
-    $sql = "SELECT * FROM clients";
-    try {
-        $db = DB::getInstance();
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    function getAllClients() {
+        $sql = "SELECT * FROM clients";
+        try {
+            $db = DB::getInstance();
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $db->query($sql);
-        $computers = $stmt->fetchAll(PDO::FETCH_OBJ);
-        DB::close();
-        echo json_encode($computers);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
+            $stmt = $db->query($sql);
+            $computers = $stmt->fetchAll(PDO::FETCH_OBJ);
+            DB::close();
+            echo json_encode($computers);
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+    };
 
 /*
 *   Get client's details
 *   @params {id} - client's id
 */
 
-function getClientDetails($request, $response, $args) {
-	$sql = "SELECT * FROM clients WHERE id='" . $args['id'] . "'";
-    try {
-        $db = DB::getInstance();
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    function getClientDetails($request, $response, $args) {
+        $sql = "SELECT * FROM clients WHERE id='" . $args['id'] . "'";
+        try {
+            $db = DB::getInstance();
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $db->query($sql);
-        $computer = $stmt->fetchAll(PDO::FETCH_OBJ);
-        DB::close();
-        echo json_encode($computer);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-};
+            $stmt = $db->query($sql);
+            $computer = $stmt->fetchAll(PDO::FETCH_OBJ);
+            DB::close();
+            echo json_encode($computer);
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+    };
 
 /*
 *   Add new client
 */
-function postAddClient($request, $response, $args) {
-	$client = json_decode($request->getBody());
-	$sql = "INSERT INTO clients (`id`, `name`, `subname`, `dni`, `photo`, `productos`)
-            VALUES (:id, :name, :subname, :dni, :photo, :productos)";
-    try {
-        $db = DB::getInstance();
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    function postAddClient($request, $response, $args) {
+        $client = json_decode($request->getBody());
+        $sql = "INSERT INTO clients (`id`, `name`, `subname`, `dni`, `photo`, `productos`)
+                VALUES (:id, :name, :subname, :dni, :photo, :productos)";
+        try {
+            $db = DB::getInstance();
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $db->prepare($sql);
-		$stmt->bindParam("id", $client->id);
-        $stmt->bindParam("name", $client->name);
-        $stmt->bindParam("subname", $client->subname);
-        $stmt->bindParam("dni", $client->dni);
-        $stmt->bindParam("photo", $client->photo);
-        $stmt->bindParam("productos", $client->productos);
-        $stmt->execute();
-        DB::close();
-        echo 1;
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-};
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam("id", $client->id);
+            $stmt->bindParam("name", $client->name);
+            $stmt->bindParam("subname", $client->subname);
+            $stmt->bindParam("dni", $client->dni);
+            $stmt->bindParam("photo", $client->photo);
+            $stmt->bindParam("productos", $client->productos);
+            $stmt->execute();
+            DB::close();
+            echo 1;
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+    };
 
 /**
 * update specific client
 * @param {id} - client's id
 */
-function putUpdateClient($request, $response, $args) {
-    $client = json_decode($request->getBody());
+    function putUpdateClient($request, $response, $args) {
+        $client = json_decode($request->getBody());
+        $sql = " UPDATE clients
+                SET name=:name, subname=:subname, dni=:dni, photo=:photo, productos=:productos
+                WHERE id='" . $args['id'] . "'";
+        try {
+            $db = DB::getInstance();
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo $client;
+            $stmt = $db->prepare($sql);
+            //$stmt->bindParam("id", $client->id);
+            $stmt->bindParam("name", $client->name);
+            $stmt->bindParam("subname", $client->subname);
+            $stmt->bindParam("dni", $client->dni);
+            $stmt->bindParam("photo", $client->photo);
+            $stmt->bindParam("productos", $client->productos);
+            $stmt->execute();
+            DB::close();
+            echo 1;
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+    };
 
-	$sql = " UPDATE clients
-			 SET name=:name, subname=:subname, dni=:dni, photo=:photo, productos=:productos
-			 WHERE id='" . $args['id'] . "'";
-    try {
-        $db = DB::getInstance();
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+/**
+* delete specific client
+* @param {id} - client id
+*/
+    function deleteClientInstance($request, $response, $args) {
+        $sql = "DELETE FROM clients
+                WHERE id='" . $args['id'] . "'";
+        try {
+            $db = DB::getInstance();
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam("id", $client->id);
-        $stmt->bindParam("name", $client->name);
-        $stmt->bindParam("subname", $client->subname);
-        $stmt->bindParam("dni", $client->dni);
-        $stmt->bindParam("photo", $client->photo);
-        $stmt->bindParam("productos", $client->productos);
-        $stmt->execute();
-        DB::close();
-        echo 1;
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-};
+            $stmt = $db->query($sql);
+            $stmt->execute();
+            DB::close();
+            echo 1;
+        } catch(PDOException $e) {
+            echo '{"error":{"text":'. $e->getMessage() .'}}';
+        }
+    };
+
 ?>
